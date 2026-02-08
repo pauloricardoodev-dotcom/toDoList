c97be00e447f2d468bc6d12553e1f5f9b3874b6d
P4UL0_O_DEV <pauloricardoodev@gmail.com>
2026-02-08 13:09:45 -0300

    Creating Admin page
    Created the admin page and added functions for buttons and other basic functionalities

--- myapp/src/pages/Home/index.js
+++ myapp/src/pages/Home/index.js
@@ -1,8 +1,66 @@
+import { useState } from 'react'
+import './home.css'
+import { Link } from 'react-router-dom'
+import { auth } from '../../firebaseConnection'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
-export default function App() {
+export default function Home() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate();
+
+   async function handleLogin(e){
+    e.preventDefault();
+
+    if(email !== '' && password !== ''){
+      
+      await signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        navigate('/admin', { replace:true })
+      })
+      .catch(() => {
+        alert("ERRO AO FAZER LOGIN")
+      })
+
+    }else{
+      alert("Preencha todos os campos!")
+    }
+  }
+
   return (
     <div>
-      <h1>Projeto To Do List!</h1>
+      <div className='home-container'>
+        <h1>To do List</h1>
+        <span>Gerencie sua agenda!</span>
+
+        <form className='form' onSubmit={handleLogin}>
+          <input 
+            type='text' 
+            placeholder='Digite seu email' 
+            value={email} 
+            onChange={
+              (e) => setEmail(e.target.value)
+            }
+          />
+
+          <input 
+            type='password' 
+            placeholder='****' 
+            value={password} 
+            onChange={
+              (e) => setPassword(e.target.value)
+            }
+          />
+
+          <button type='submit'>Acessar</button>
+        </form>
+
+        <Link className='button-link' to="/register">
+          não possuí uma conta? cadastre-se agora!
+        </Link>
+      </div>
     </div>
   );
 }
